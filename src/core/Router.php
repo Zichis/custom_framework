a9cba773eb473b2afa6153f0570b7618ce6d0195
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-23 23:23:18 +0100

    Router class updated with methods.

--- src/core/Router.php
+++ src/core/Router.php
@@ -7,9 +7,27 @@ namespace App\Core;
  */
 class Router
 {
+  protected array $routes = [];
+  protected Request $request;
 
-  function __construct()
+  function __construct(Request $request)
   {
-    //
+    $this->request = $request;
+  }
+
+  public function get($path, $callback)
+  {
+    $this->routes['get'][$path] = $callback;
+  }
+
+  public function resolve()
+  {
+    $path = $this->request->getPath();
+    $method = $this->request->getMethod();
+    echo "<pre>";
+    var_dump($path);
+    var_dump($method);
+    echo "</pre>";
+    exit;
   }
 }
