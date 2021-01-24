2fea7943ae331e7db30b41eff73aade1d79aeb74
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-24 13:51:41 +0100

    Updated index file.

--- public/index.php
+++ public/index.php
@@ -4,11 +4,7 @@ require_once '../vendor/autoload.php';
 
 use App\Core\Application;
 
-$app = new Application();
-$app->router->get('/', function(){
-  return '<h1>Index</h1>';
-});
-$app->router->get('/home', function(){
-  return '<h1>Home</h1>';
-});
-$app->router->resolve();
+$app = new Application(dirname(__DIR__));
+$app->router->get('/', 'home');
+$app->router->get('/users', 'users');
+$app->run();
