4012754a44e8b9815dc28fd079c39997d90d060d
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-25 23:48:17 +0100

    POST and GET route created for contact.

--- public/index.php
+++ public/index.php
@@ -7,4 +7,8 @@ use App\Core\Application;
 $app = new Application(dirname(__DIR__));
 $app->router->get('/', 'home');
 $app->router->get('/users', 'users');
+$app->router->get('/contact', 'contact');
+$app->router->post('/contact', function(){
+  return "Handling submitted data";
+});
 $app->run();
