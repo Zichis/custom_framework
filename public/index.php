fd744231231884cad471cc7c365a4a4622fe5b11
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-26 23:49:32 +0100

    Changed route format.

--- public/index.php
+++ public/index.php
@@ -3,12 +3,11 @@
 require_once '../vendor/autoload.php';
 
 use App\Core\Application;
+use App\Controllers\ContactController;
 
 $app = new Application(dirname(__DIR__));
 $app->router->get('/', 'home');
 $app->router->get('/users', 'users');
 $app->router->get('/contact', 'contact');
-$app->router->post('/contact', function(){
-  return "Handling submitted data";
-});
+$app->router->post('/contact', [ContactController::class, 'send']);
 $app->run();
