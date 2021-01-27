b835186780206f81375c3441befa5963422b8b88
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-27 14:46:43 +0100

    Created pages controller.

---
+++ src/controllers/PagesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * @author Ezichi Ebere
+ */
+class PagesController
+{
+  public function index()
+  {
+    return "We are in pages controller!";
+  }
+}
