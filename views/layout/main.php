55c227387780a37e01921c99baf970984e828db3
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-28 10:47:03 +0100

    Added contact link to navbar.

--- views/layout/main.php
+++ views/layout/main.php
@@ -25,6 +25,9 @@
             <li class="nav-item">
               <a class="nav-link" href="/users">Users</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact">Contact</a>
+            </li>
           </ul>
         </div>
       </div>
