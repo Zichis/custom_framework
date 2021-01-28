45e7f6127bbdc123a65b472029821a4ee4c6b959
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-01-28 10:40:03 +0100

    Used helper method isPost() in controller.

--- src/controllers/ContactController.php
+++ src/controllers/ContactController.php
@@ -12,6 +12,10 @@ class ContactController
 {
   public function send(Request $request)
   {
+    if ($request->isPost()) {
+      echo "It is post!";
+      die;
+    }
     var_dump($request->getBody());
     die;
     return "We are in controller!";
