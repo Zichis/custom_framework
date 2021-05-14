77a48dde11474ae24dedd928563ebcc2846c5552
Ezichi Ebere <tripleee@yahoo.co.uk>
2021-05-14 23:42:43 +0100

    Added UsersController class to route.

--- public/index.php
+++ public/index.php
@@ -5,10 +5,11 @@ require_once '../vendor/autoload.php';
 use App\Core\Application;
 use App\Controllers\ContactController;
 use App\Controllers\PagesController;
+use App\Controllers\UsersController;
 
 $app = new Application(dirname(__DIR__));
 $app->router->get('/', [PagesController::class, 'index']);
-$app->router->get('/users', 'users');
+$app->router->get('/users', [UsersController::class, 'index']);
 $app->router->get('/contact', [ContactController::class, 'index']);
 $app->router->post('/contact', [ContactController::class, 'index']);
 $app->run();
